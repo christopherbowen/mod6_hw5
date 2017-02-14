cf8f1cef09d912722fa9db681c5aac8c5d31cc5c
Wei Dong <wd47878@icarus.cs.weber.edu>
2017-02-14 17:14:32 +0000

    first commit

---
+++ neutrons_hw5_task2.c
@@ -0,0 +1,28 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  neutrons_hw5_task1.c
+ *       	Usage:  ./neutrons_hw5_task1.c
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  02/14/2017 04:38:00 PM
+ *       Compiler:  gcc -Wall -Werror
+ *         Author:  Christopher Bowen (), christopherbowen@mail.weber.edu
+ * =====================================================================================
+ */
+#include <stdio.h>		/* For Standard I/O */
+
+/* Function Prototypes */
+
+/* Main Program */
+int main(void)
+{
+
+	return 0;
+}
+
+
+/* Function Defenitions */
+
+
