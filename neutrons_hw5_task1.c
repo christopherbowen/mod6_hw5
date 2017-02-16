184c02cbff843837099cdfb089c7af13728b3487
christopher bowen <christopherbowen@mail.weber.edu>
2017-02-16 17:20:16 +0000

    final copy

--- neutrons_hw5_task1.c
+++ neutrons_hw5_task1.c
@@ -3,7 +3,7 @@
  *
  *       Filename:  neutrons_hw5_task1.c
  *       	Usage:  ./neutrons_hw5_task1.c
- *    Description:  
+ *    Description: the calculation for the second moment of inertia  
  *
  *        Version:  1.0
  *        Created:  02/14/2017 04:38:00 PM
@@ -14,14 +14,25 @@
 #include <stdio.h>		/* For Standard I/O */
 
 /* Function Prototypes */
-
+float AnnulusMoment(float r2, float r1);
 /* Main Program */
 int main(void)
 {
-
+	float r2, r1;
+	printf("please insert the number for your outer radius: ");
+	scanf("%f", &r2);
+	printf("please insert the number for your inner radius: ");
+	scanf("%f", &r1);
+	float I = AnnulusMoment(r2,r1);
+	printf("the second moment of inertia is %.2f\n", I);
 	return 0;
 }
-
+float AnnulusMoment(float i, float j)
+{
+	float f=0;
+	f = (3.14/4)*((i*i*i*i)-(j*j*j*j));
+	return f;
+}
 
 /* Function Defenitions */
 
